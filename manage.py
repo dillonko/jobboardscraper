bec36cc227d2a90e3e08e3fa9ca492acf9c6c982
Richard Cornish <richardcornish@gmail.com>
2014-08-17 14:57:52 -0500

    Adding production settings to manage to debug 500

--- manage.py
+++ manage.py
@@ -2,15 +2,10 @@
 import os
 import sys
 
-from django.conf import settings
-
 
 if __name__ == "__main__":
 
-    if getattr(settings, 'DEBUG', False):
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.production")
-    else:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.local")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.production")
 
     from django.core.management import execute_from_command_line
 
