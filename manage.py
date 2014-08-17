88aafe51fcb569f16d992cbf88125bba7d36d2bd
Richard Cornish <richardcornish@gmail.com>
2014-08-16 22:42:16 -0500

    Changed manage.py for local and production

--- manage.py
+++ manage.py
@@ -2,8 +2,15 @@
 import os
 import sys
 
+from django.conf import settings
+
+
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.local")
+
+    if getattr(settings, 'DEBUG', False):
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.production")
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.local")
 
     from django.core.management import execute_from_command_line
 
