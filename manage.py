121d5b608ff6ae61bc17e4b32e7cfc8f098c6745
Richard Cornish <richardcornish@gmail.com>
2014-08-17 15:45:42 -0500

    Setting manage.py back to local because production should be an environment variable on production server

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == "__main__":
 
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.production")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.local")
 
     from django.core.management import execute_from_command_line
 
