fb3085354b2fd7c58f4172b31615f2cdd89fa48c
Richard Cornish <richardcornish@gmail.com>
2014-08-18 01:25:51 -0500

    Removing unnecessary and error-prone DSM from Scrapy settings

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -17,16 +17,6 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 #USER_AGENT = 'scraper (+http://www.yourdomain.com)'
 
 
-# Set DJANGO_SETTINGS_MODULE
-from django.conf import settings
-if getattr(settings, 'DEBUG', False):
-    import os, sys
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.local")
-else:
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "timgorin.settings.production")
-    path = os.path.join(os.path.dirname(__file__),'../timgorin')
-    sys.path.append(os.path.abspath(path))
-
 ITEM_PIPELINES = {
     'scraper.pipelines.ScraperPipeline': 300,
 }
