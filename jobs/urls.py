e76e10e9a519d75965478ccdf9422290b0c6525a
Richard Cornish <richardcornish@gmail.com>
2015-01-11 21:27:20 -0600

    Went back to simpler query-string pagination

--- jobs/urls.py
+++ jobs/urls.py
@@ -9,12 +9,6 @@ urlpatterns = patterns('jobs.views',
         name='job_detail'
     ),
 
-    # List, paginated
-    url(r'^page/(?P<page>[0-9]+)/$',
-        view='job_list',
-        name='job_list_pages'
-    ),
-
     # List
     url(r'^$',
         view='job_list',
