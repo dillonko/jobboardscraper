4a104e60e58fe2c56ea02b2d27bc8bffae221eb6
Richard Cornish <richardcornish@gmail.com>
2015-03-27 17:02:19 -0500

    Moving Django settings after Django setup runs

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -20,11 +20,6 @@ ITEM_PIPELINES = {
     'scraper.pipelines.EslCafePipeline': 300,
 }
 
-
-# Set Django settings module
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'timgorin.settings'
-
 # Although not documented anywhere, I *think* Scrapy falls under the
 # use case of `AppRegistryNotReady` error "if you forget to call
 # django.setup() in a standalone Python script."
@@ -32,3 +27,7 @@ os.environ['DJANGO_SETTINGS_MODULE'] = 'timgorin.settings'
 
 import django
 django.setup()
+
+# Set Django settings module
+import os
+os.environ['DJANGO_SETTINGS_MODULE'] = 'timgorin.settings'
