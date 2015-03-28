fdf38220c93e3335bc51ab2fdd975d554ee06bc6
Richard Cornish <richardcornish@gmail.com>
2015-03-27 22:51:37 -0500

    Moving Django setup earlier in scrapy settings

--- scraper/scraper/settings.py
+++ scraper/scraper/settings.py
@@ -19,18 +19,3 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 ITEM_PIPELINES = {
     'scraper.pipelines.EslCafePipeline': 300,
 }
-
-# Although not documented anywhere, I *think* Scrapy falls under the
-# use case of `AppRegistryNotReady` error "if you forget to call
-# django.setup() in a standalone Python script."
-# https://docs.djangoproject.com/en/1.7/ref/applications/#troubleshooting
-
-import django
-django.setup()
-
-# Set Django settings module
-import sys
-sys.path.append('/app/timgorin')
-
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'timgorin.settings'
