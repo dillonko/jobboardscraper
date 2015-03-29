877baab1537be0525ec4e105ff3e612a41b6ee72
Richard Cornish <richardcornish@gmail.com>
2015-03-29 02:57:47 -0500

    Trying again

--- timgorin/search/views.py
+++ timgorin/search/views.py
@@ -10,7 +10,7 @@ class MySearchView(SearchView):
         Copied from django-haystack to simply customize the `search_form` context variable
         https://github.com/toastdriven/django-haystack/blob/master/haystack/views.py#L126
         """
-        (paginator, page, page_obj) = self.build_page()
+        (paginator, page) = self.build_page()
 
         context = {
             'query': self.query,
