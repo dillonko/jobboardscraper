043473ea7478f4ebbcbb96df28967407bcb5190d
Richard Cornish <richardcornish@gmail.com>
2015-03-28 23:30:52 -0500

    Trying again

--- timgorin/scraper/settings.py
+++ timgorin/scraper/settings.py
@@ -19,18 +19,3 @@ NEWSPIDER_MODULE = 'scraper.spiders'
 ITEM_PIPELINES = {
     'scraper.pipelines.EslCafePipeline': 300,
 }
-
-
-# Django Item requires explicit Django settings in Scrapy settings
-# http://doc.scrapy.org/en/latest/topics/djangoitem.html
-
-import os
-os.environ['DJANGO_SETTINGS_MODULE'] = 'timgorin.settings'
-
-# Although not documented anywhere, I *think* Scrapy falls under the
-# use case of `AppRegistryNotReady` error "if you forget to call
-# django.setup() in a standalone Python script."
-# https://docs.djangoproject.com/en/1.7/ref/applications/#troubleshooting
-
-import django
-django.setup()
