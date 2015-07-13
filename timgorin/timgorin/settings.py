94d93139e88687a031ad4d133a7c5e47ff42c81b
Richard Cornish <richardcornish@gmail.com>
2015-07-12 21:19:30 -0500

    Request context processor is now under templates

--- timgorin/timgorin/settings.py
+++ timgorin/timgorin/settings.py
@@ -132,7 +132,6 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.core.context_processors.request',
                 'timgorin.context_processors.search_form',
             ],
         },
