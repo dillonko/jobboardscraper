674557b8cbd085942ffcfd989aa9fd7af7d4ecc9
Richard Cornish <richardcornish@gmail.com>
2015-07-12 19:15:03 -0500

    Prepopulated orgs titles aren't needed because admin isn't used

--- timgorin/organizations/admin.py
+++ timgorin/organizations/admin.py
@@ -5,7 +5,6 @@ from .models import Organization
 
 class OrganizationAdmin(admin.ModelAdmin):
     list_display = ('title',)
-    prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Organization, OrganizationAdmin)
