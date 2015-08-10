756c9d7c3cefc6b2d0301ec4bfbdc16563dec453
Richard Cornish <richardcornish@gmail.com>
2015-08-10 15:40:03 -0500

    Search forms are always unbound because they're GET requests

--- timgorin/timgorin/context_processors.py
+++ timgorin/timgorin/context_processors.py
@@ -2,7 +2,6 @@ from search.forms import MySearchForm
 
 
 def search_form(request):
-    """
-    Gets unbound search form class
-    """
-    return {'search_form': MySearchForm()}
+    return {
+        'search_form': MySearchForm()
+    }
