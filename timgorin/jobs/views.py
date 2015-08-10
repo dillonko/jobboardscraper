5a3ca8bf7fb8fe675c81497db2c5d6bc3e887ff0
Richard Cornish <richardcornish@gmail.com>
2015-08-10 15:43:31 -0500

    Paginator context already includes total count of objects

--- timgorin/jobs/views.py
+++ timgorin/jobs/views.py
@@ -13,8 +13,3 @@ class JobDetailView(DetailView):
 class JobListView(PaginationMixin, ListView):
     model = Job
     paginate_by = getattr(settings, 'PAGINATE_BY', 10)
-
-    def get_context_data(self, **kwargs):
-        context = super(JobListView, self).get_context_data(**kwargs)
-        context['job_count'] = Job.objects.count()
-        return context
--- timgorin/organizations/views.py
+++ timgorin/organizations/views.py
@@ -8,16 +8,7 @@ from .models import Organization
 class OrganizationDetailView(DetailView):
     model = Organization
 
-organization_detail = OrganizationDetailView.as_view()
-
 
 class OrganizationListView(PaginationMixin, ListView):
     model = Organization
     paginate_by = 50
-
-    def get_context_data(self, **kwargs):
-        context = super(OrganizationListView, self).get_context_data(**kwargs)
-        context['organization_count'] = Organization.objects.count()
-        return context
-
-organization_list = OrganizationListView.as_view()
