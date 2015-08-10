0e134e0a1cb93951d949b3f93f22465b67b74e9c
Richard Cornish <richardcornish@gmail.com>
2015-08-10 15:42:40 -0500

    Search form is already available from context processor

--- timgorin/search/views.py
+++ timgorin/search/views.py
@@ -9,8 +9,3 @@ from .forms import MySearchForm
 class MySearchView(PaginationMixin, SearchView):
     form_class = MySearchForm
     paginate_by = getattr(settings, 'PAGINATE_BY', 10)
-
-    def get_context_data(self, *args, **kwargs):
-        context = super(MySearchView, self).get_context_data(*args, **kwargs)
-        context['search_form'] = self.get_form_class()
-        return context
