bd13c45721c82062d1c048c34eaef3614656679c
Richard Cornish <richardcornish@gmail.com>
2016-08-18 20:01:25 -0500

    Updated settings comment

--- jobboardscraper/jobboardscraper/settings.py
+++ jobboardscraper/jobboardscraper/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
